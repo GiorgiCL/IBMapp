e7935b1d3e514d82cc6e76ce587f130055ed06db
GiorgiCL <s054865@ad.viko.lt>
2026-03-13 17:37:21 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/giorgi/ibmapp/IbmappApplication.java
@@ -0,0 +1,13 @@
+package com.giorgi.ibmapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IbmappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/giorgi/ibmapp/IbmappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giorgi.ibmapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbmappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
